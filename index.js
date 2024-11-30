270fff1b5839f04a3962420668c0decf8d9877f1
flavin-jr <fjcvj@cin.ufpe.br>
2024-11-30 16:40:21 -0300

    feat: add route for delete task

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(express.json())
 connectToDatabase();
+
 app.get('/tasks', async (req, res) => {
     const tasks = await TaskModel.find({})
     res.status(200).send(tasks)
@@ -18,6 +19,12 @@ app.post('/tasks', async (req, res) => {
     const newTask = new TaskModel(req.body);
     await newTask.save();
     res.status(201).send(newTask);
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    const taskId = req.params.id;
+    await TaskModel.findByIdAndDelete(taskId)
+    res.status(200).send(`task id ${taskId} deleted!!!`);
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
