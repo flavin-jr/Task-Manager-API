548c1c8f071caf48b2dea5fd43f809a9a5baa663
flavin-jr <fjcvj@cin.ufpe.br>
2024-11-30 16:18:05 -0300

    feat: create task route

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const connectToDatabase = require("./src/database/mongoose.database")
 dotenv.config();
 const app = express();
 
-
+app.use(express.json())
 connectToDatabase();
 app.get('/tasks', async (req, res) => {
     const tasks = await TaskModel.find({})
     res.status(200).send(tasks)
 });
+
+app.post('/tasks', async (req, res) => {
+    const newTask = new TaskModel(req.body);
+    await newTask.save();
+    res.status(201).send(newTask);
+})
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
