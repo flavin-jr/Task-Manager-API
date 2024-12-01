ffe700c32a18ad63064408e2159080f8ae5a8718
flavin-jr <fjcvj@cin.ufpe.br>
2024-12-01 00:44:24 -0300

    feat: add patch request to change the task

--- index.js
+++ index.js
@@ -11,26 +11,71 @@ app.use(express.json())
 connectToDatabase();
 
 app.get('/tasks', async (req, res) => {
-    const tasks = await TaskModel.find({})
-    res.status(200).send(tasks)
+    try {
+        const tasks = await TaskModel.find({});
+
+        return res.status(200).send(tasks);
+    }
+    catch (error) {
+        return res.status(500).send(error.message);
+    }
 });
 
 app.get('/tasks/:id', async (req, res) => {
-    const taskId = req.params.id;
-    const task = await TaskModel.findById(taskId);
+    try {
+        const taskId = req.params.id;
 
-    res.status(200).send(task)
+        const task = await TaskModel.findById(taskId);
+        if (!task) {
+            return res.status(404).send('task nao encontrada');
+        }
+        return res.status(200).send(task);
+    }
+    catch (error) {
+        return res.status(500).send(error.message)
+    }
+
 })
 app.post('/tasks', async (req, res) => {
-    const newTask = new TaskModel(req.body);
-    await newTask.save();
-    res.status(201).send(newTask);
-})
+    try {
+        const newTask = new TaskModel(req.body);
+        await newTask.save();
 
+        return res.status(201).send(newTask);
+    }
+    catch (error) {
+        return res.status(500).send(error.message);
+    }
+})
+app.patch('/tasks/:id', async (req, res) => {
+    try {
+
+        const taskId = req.params.id;
+        const taskData = req.body;
+        const taskToUpdate = await TaskModel.findById(taskId)
+        const allowedUpdate = ['isCompleted'];
+        const requestedUpdates = Object.keys(req.body);
+
+        for (update of requestedUpdates) {
+            if (allowedUpdate.includes(update)) {
+                taskToUpdate[update] = taskData[update]
+            }
+            else {
+                return res.status(500).send('Um ou mais campos inseridos nao sao editaveis')
+            }
+        }
+
+        await taskToUpdate.save()
+        return res.status(200).send(taskToUpdate);
+    }
+    catch (error) {
+        return res.status(500).send(error.message);
+    }
+})
 app.delete('/tasks/:id', async (req, res) => {
     const taskId = req.params.id;
     await TaskModel.findByIdAndDelete(taskId)
-    res.status(200).send(`task id ${taskId} deleted!!!`);
+    return res.status(200).send(`task id ${taskId} deleted!!!`);
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
